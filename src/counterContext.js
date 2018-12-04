8425c4a5b021082db17014ee625c0c4fb5eb62b4
a.atalla <a.3talla@gmail.com>
2018-12-04 09:24:22 +0400

    move methods to the state, use prevState

--- src/counterContext.js
+++ src/counterContext.js
@@ -6,35 +6,30 @@ export class CounterProvider extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      counter: 0,
+      incrementCounter: this.incrementCounter,
+      decrementCounter: this.decrementCounter
     }
   }
 
-  incrementCounter = () => {
-    const { counter } = this.state;
-    this.setState({
-      counter: counter + 1
-    })
+  incrementCounter = () => {    
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1
+    }));
   }
 
   decrementCounter = () => {
     const { counter } = this.state;
     if(counter > 0) {
-      this.setState({
-        counter: this.state.counter - 1
-      });
+      this.setState((prevState) => ({
+        counter: prevState.counter - 1
+      }));
     }
   }
 
   render() {
     return (
-      <Provider
-        value={{
-          ...this.state,
-          incrementCounter: this.incrementCounter,
-          decrementCounter: this.decrementCounter
-        }}
-      >
+      <Provider value={this.state}>
         {this.props.children}
       </Provider>
     )
